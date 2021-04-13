fb7ff2677566d8c2059bc9b89353978860516d37
e30rick <80066755+e30rick@users.noreply.github.com>
2021-04-12 19:34:15 -0600

    Version 1.7

--- src/main/java/GeneradorSuelos/TrazadorSuelo.java
+++ src/main/java/GeneradorSuelos/TrazadorSuelo.java
@@ -1,4 +1,3 @@
-
 package GeneradorSuelos;
 
 import java.awt.Color;
@@ -9,33 +8,32 @@ import javax.swing.JOptionPane;
  * @author e30ri
  */
 public class TrazadorSuelo {
-    
-       //El arreglo debe estar ligado a la clase que contiene las caracteristicas del objeto
-    public static SueloIndicador[][] creadorSueloIncial(int CasillasX , int CasillasY){
-    
-        SueloIndicador celdaSuelo; //variable que se llamara continuamente
-        
-        SueloIndicador[][] suelo1 = new SueloIndicador[CasillasX][CasillasY];
-        for(int i = 0; i < CasillasX; i++){
-        for(int j = 0; j < CasillasY; j++){
-        
-           CreadorSuelos jLabel1 = new CreadorSuelos(i+1, j+1,"Fila:" + (i + 1) + " Columna:" + (j + 1) );
-           jLabel1.setOpaque(true);
-           jLabel1.setBackground(Color.BLUE);
-           jLabel1.addMouseListener(new java.awt.event.MouseAdapter(){
-           @Override
-           public void mouseClicked(java.awt.event.MouseEvent evt){
-           CreadorSuelos label=(CreadorSuelos)evt.getComponent();
-                        JOptionPane.showMessageDialog(null,"Fila:"+(label.nFilas) +" Columna:"+(label.nColumnas));
-           }
-           });
-         celdaSuelo = new SueloIndicador(EnumSuelos.AGUA, jLabel1,false);
-                 suelo1[i][j] = celdaSuelo;
 
+    //El arreglo debe estar ligado a la clase que contiene las caracteristicas del objeto
+    public static SueloIndicador[][] creadorSueloIncial(int CasillasX, int CasillasY) {
+
+        SueloIndicador celdaSuelo; //variable que se llamara continuamente
+
+        SueloIndicador[][] suelo1 = new SueloIndicador[CasillasX][CasillasY];
+        for (int i = 0; i < CasillasX; i++) {
+            for (int j = 0; j < CasillasY; j++) {
+
+                CreadorSuelos jLabel1 = new CreadorSuelos(i + 1, j + 1, "Fila:" + (i + 1) + " Columna:" + (j + 1));
+                jLabel1.setOpaque(true);
+                jLabel1.setBackground(Color.BLUE);
+                jLabel1.addMouseListener(new java.awt.event.MouseAdapter() {
+                    @Override
+                    public void mouseClicked(java.awt.event.MouseEvent evt) {
+                        CreadorSuelos label = (CreadorSuelos) evt.getComponent();
+                        JOptionPane.showMessageDialog(null, "Fila:" + (label.nFilas) + " Columna:" + (label.nColumnas));
+                    }
+                });
+                celdaSuelo = new SueloIndicador(EnumSuelos.AGUA, jLabel1, false);
+                suelo1[i][j] = celdaSuelo;
+
+            }
         }
+        return suelo1;
     }
-       return suelo1;
-  }
-      
-    
+
 }
